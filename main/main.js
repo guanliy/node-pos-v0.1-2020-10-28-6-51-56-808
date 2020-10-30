784951ee2d0334a80e7c79d7c82e4c4c801cf25c
guanliy <gly1990@126.com>
2020-10-30 13:37:16 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -2,3 +2,26 @@ module.exports = function main() {
     console.log("Debug Info");
     return 'Hello World!';
 };
+
+function printReceipt(arr) { 
+
+    var obj = arr;
+
+    for (var i = 0; i < obj.length; i++) {
+        console.log("Name " + i + ": " + obj[i].Barcode);
+    }
+    
+    let data = JSON.stringify(arr);  
+    
+    fs.writeFileSync('file.json', data, finished);
+    
+    $.getJSON('file.json', function (data) {
+        
+        var items = data.response.venue.receipts.items;           
+    
+        console.log("Name:" + name + "Quantity:" + Quantity+'Unit price:'+UnitPrice+'Subtotal:'+Subtotal);
+        
+    });
+ 
+   
+  }
